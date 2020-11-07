ce5f3145bf8a311094aaf0bd7357085af8a8b4dc
peedrojs <developer.peedrojs@gmail.com>
2020-11-07 17:41:36 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold, useFonts } from '@expo-google-fonts/nunito'
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <Routes />
+  );
+}
